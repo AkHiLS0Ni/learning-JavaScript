431d72744fc3c3fc865aece7ac37eb401ff6fdb9
Akhil Soni <143425529+AkHiLS0Ni@users.noreply.github.com>
2025-07-10 12:04:10 +0530

    conversions of data types

---
+++ conversion.js
@@ -0,0 +1,35 @@
+let score = "44"
+
+console.log (typeof score); // "string"
+console.log(typeof(score)); // "string"
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // "number"
+console.log(valueInNumber); // 44
+
+// "33abc" is not a valid number
+// true =>1; // false => 0
+//"44" => 44 
+
+
+
+let isLoggedIn = true;// isLoggedIn = false; // true => 1; false => 0
+console.log(typeof isLoggedIn); // "boolean"
+console.log(isLoggedIn); // true    
+
+
+
+
+let isLoggendIn = 1// isLoggedIn = 0; // true => 1; false => 0
+
+let booleanIsLoggedIn = Boolean(isLoggendIn);
+console.log(typeof booleanIsLoggedIn); // "boolean"
+
+
+
+
+let SomeNumber = 55;
+
+let stringNumber = String(SomeNumber);
+console.log(typeof stringNumber); // "string"   
+console.log(stringNumber); // "55"
