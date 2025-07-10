85fa4df3f4f092a07c15f3fa2c044de16be8edd0
Akhil Soni <143425529+AkHiLS0Ni@users.noreply.github.com>
2025-07-10 09:53:12 +0530

    data type.js

---
+++ data type.js
@@ -0,0 +1,27 @@
+
+console.log(3+3)
+
+console.log ("Hello World")
+
+let name = "akhil soni"
+let age = 23 
+
+/*
+number => 2 to power 53
+BigInt
+string=>""
+boolean => true/false
+null=> standalone value
+undefined => no value assigned
+Symbol => unique value
+
+*/
+// object => {key:value, key:value}
+ console.log (typeof age)
+ console.log (typeof name)  
+console.log (typeof true)
+console.log (typeof null) // null is an object in JS
+console.log (typeof undefined)  // undefined is a type in JS
+console.log (typeof Symbol("id")) // symbol is a type in JS 
+
+console.log (typeof 3n) // BigInt is a type in JS
