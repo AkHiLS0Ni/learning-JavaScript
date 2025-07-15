2c8370ca8cd311c675f4f1db5db5ceb260a74450
Akhil Soni <143425529+AkHiLS0Ni@users.noreply.github.com>
2025-07-15 10:32:47 +0530

    Comparison of datatypes

--- Comparison of datatypes.js
+++ Comparison of datatypes.js
@@ -8,5 +8,26 @@ console,log(2 != 1); // true
 
 */
 
-console.log("2">1); // true
-console.log("02"<1); // false
+/*console.log("2">1); // true
+console.log("02"<1); // false*/
+
+// try to avoid using console,log instead of console.log
+
+
+console.log(2 > 1); // true
+console.log(null == 0); // false
+console.log(null == undefined); // true
+console.log(null >= 0); // true
+console.log(null <= 0); // true
+console.log(undefined == 0); // false
+console.log(undefined == null); // true 
+console.log(undefined >= 0); // false
+console.log(undefined <= 0); // false
+console.log("2" == 2); // true
+console.log("2" === 2); // false    
+console.log("2" != 2); // false
+console.log("2" !== 2); // true 
+
+// === 
+console.log("2" === 2); // false
+
